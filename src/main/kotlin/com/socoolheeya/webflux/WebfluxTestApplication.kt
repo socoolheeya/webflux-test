19407d80e8b3e84103dff32dad6337ed6bf29287
wonhee.lee <wonhee.lee@tidesquare.com>
2024-03-01 18:45:47 +0900

    Feat: 프로젝트 생성 최초 커밋

---
+++ src/main/kotlin/com/socoolheeya/webflux/WebfluxTestApplication.kt
@@ -0,0 +1,11 @@
+package com.socoolheeya.webflux
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebfluxTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<WebfluxTestApplication>(*args)
+}
