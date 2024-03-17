6822bf2bdc5736b6896b3c18e50ee0da3cbe115e
wonhee.lee <lwh0102@gmail.com>
2024-03-17 23:44:17 +0900

    :sparkles: Fix: 전체 멤버 조회 기능 추가

--- src/main/kotlin/com/socoolheeya/webflux/member/adapter/in/external/MemberController.kt
+++ src/main/kotlin/com/socoolheeya/webflux/member/adapter/in/external/MemberController.kt
@@ -2,10 +2,12 @@ package com.socoolheeya.webflux.member.adapter.`in`.external
 
 import com.socoolheeya.webflux.member.adapter.out.external.MemberResponse
 import com.socoolheeya.webflux.member.application.service.MemberService
+import com.socoolheeya.webflux.member.domain.Member
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @RestController
 @RequestMapping("/v1/members")
@@ -13,8 +15,13 @@ class MemberController(
     private val memberService: MemberService
 ) {
 
+    @GetMapping("")
+    suspend fun getMembers(): MutableList<Member> {
+        return memberService.getMembers()
+    }
+
     @GetMapping("/{memberId}")
-    fun getNewMember(@PathVariable memberId: Long): MemberResponse.Companion.Search {
+    suspend fun getNewMember(@PathVariable memberId: Long): MemberResponse.Companion.Search {
         return memberService.load(memberId)
     }
 }
