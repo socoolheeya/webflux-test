18c84d4babf7b7fdfb60f516fad66a2b8cf11986
wonhee.lee <lwh0102@gmail.com>
2024-03-20 00:01:56 +0900

    :white_check_mark: Test: 테스트 코드 수정

--- src/test/kotlin/com/socoolheeya/webflux/MemberTest.kt
+++ src/test/kotlin/com/socoolheeya/webflux/MemberTest.kt
@@ -18,7 +18,7 @@ import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.reactive.server.WebTestClient
-import java.util.*
+import java.util.UUID
 
 
 @ExtendWith(value = [SpringExtension::class])
